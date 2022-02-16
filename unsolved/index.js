7bed88a4c0f1d91a3090becd2d999e87a6fd61c9
Kaitlin Shumake <shumakek@ufl.edu>
2022-02-16 18:47:15 -0500

    added content from lecture

--- unsolved/index.js
+++ unsolved/index.js
@@ -10,18 +10,23 @@ function getPizzaOrder() {
   alert("Hi, Thanks for ordering with Web to Pizza!")
   var pizza = {
     // set the 'cost' property to the basePrice
-    // YOUR CODE HERE
+    cost: basePrice,
   }
 
   var crust = prompt(
     `Please enter the type of crust (${formatPrice(thickCrustUpcharge)} upcharge for Thick crust)`
   )
   // set the pizza object's 'crust' property to the user's response
+  pizza.crust = crust,
   // HINT: prompt() returns a string
   // HINT: You may wish to use .toLowerCase() and .trim()
   // if the user specified 'thick' crust, add thickCrustUpcharge
+  if (pizza.crust === 'thick') {
+    pizza.cost += thickCrustUpcharge
+  }
   // to pizza.cost
-  // YOUR CODE HERE
+
+  
 
   var toppings = prompt("Please enter additional toppings (comma separated)")
   // HINT: prompt() will return an empty string "" if the user presses 'OK' without entering a value
